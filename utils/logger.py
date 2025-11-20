409faeb147ef351215c5c6c69401907bcda95c7b
max <maxinlord777@gmail.com>
2025-11-20 14:48:12 +0300

    fix log

--- utils/logger.py
+++ utils/logger.py
@@ -1,5 +1,13 @@
+import datetime
 import logging
 import sys
+
+try:  # Используем timezone базы системы, иначе фиксированный сдвиг
+    from zoneinfo import ZoneInfo
+
+    MOSCOW_TZ = ZoneInfo("Europe/Moscow")
+except Exception:
+    MOSCOW_TZ = datetime.timezone(datetime.timedelta(hours=3))
 
 
 def setup_logger() -> None:
@@ -9,6 +17,7 @@ def setup_logger() -> None:
     logging.getLogger("schedule").setLevel(logging.WARNING)
     # Формат логов
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    formatter.converter = lambda timestamp: datetime.datetime.fromtimestamp(timestamp, MOSCOW_TZ).timetuple()
 
     # Обработчик для вывода в консоль
     console_handler = logging.StreamHandler(sys.stdout)
